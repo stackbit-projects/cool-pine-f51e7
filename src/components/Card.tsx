e74bfbeed5eb06ce8171a627c3b3899081fd4d44
Vitaliy Ribachenko <vit@ribachenko.com>
2021-11-30 12:38:59 +0200

    Fix annotations

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,7 +15,7 @@ export default function Card(props) {
             <h2 data-sb-field-path=".title">{title}</h2>
             {subtitle && <p data-sb-field-path=".subtitle">{subtitle}</p>}
             {actions.length && (
-              <div>
+              <div data-sb-field-path=".actions">
                 {actions.map((action, index) => <Action key={index} {...action} data-sb-field-path={`.${index}`} />)}
               </div>
             )}
